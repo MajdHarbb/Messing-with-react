b74e458f992fcac1e5e605c11af69944a68c44dc
Majd Harb <97592562+MajdHarbb@users.noreply.github.com>
2023-04-11 17:25:35 +0300

    alert dialog

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,15 +1,4 @@
 import { axiosClient } from "./ApiClient";
-// export function getProduct(){
-//     return axiosClient.get('/product');
-// }
-
-export function signin(data) {
-  return axiosClient.post("/login", data);
-}
-
-export function register(data) {
-  return axiosClient.post("/register", data);
-}
 
 export const api = {
   hello: () => {
